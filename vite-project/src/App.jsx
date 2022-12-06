237557ddc79246c171750a727b1c4ae3c6ebc983
Aayush99910 <amaharjann01@gmail.com>
2022-12-06 15:28:19 +0545

    adding navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar"; 
 import MainComponent from "./components/Main";
+import './assets/App.css';
 
 
 export default function App() {
--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -4,9 +4,13 @@ import logo from '../assets/react.svg'
 export default function NavBar() {
   return (
     <nav className="nav">
-      <img src={logo} alt='React Img'></img>
-      <p>React Facts</p>
-      <p>React Course- Project 1</p>
+      <div className="logo-container">
+        <img src={logo} alt='React Img'></img>
+        <p>React Facts</p>
+      </div>
+      <div className="project-number">
+        <p>React Course- Project 1</p>
+      </div>
     </nav>
   )
 }
