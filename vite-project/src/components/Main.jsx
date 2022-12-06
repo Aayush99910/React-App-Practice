25d291886b17629b630d9b437cb18d9b1193f77c
Aayush99910 <amaharjann01@gmail.com>
2022-12-06 15:55:10 +0545

    adding main bod

--- vite-project/src/components/Main.jsx
+++ vite-project/src/components/Main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function MainComponent() {
   return (
-    <main>
-      <h1>Fun facts about React</h1>
-      <ul>
+    <main className="main-body-content-container">
+      <h1 className="facts-title">Fun facts about React</h1>
+      <ul className="facts-points">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on Github</li>
