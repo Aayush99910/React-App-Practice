51825c1a7bc3b7b582f429844a4bf593a2bb2329
Aayush99910 <amaharjann01@gmail.com>
2022-12-06 19:22:39 +0545

    changing occupation

--- Solo-Project-1/src/components/Info.jsx
+++ Solo-Project-1/src/components/Info.jsx
@@ -6,7 +6,7 @@ export default function Info() {
     <div className="photo-name-container">
       <div className="profile-picture"></div>
       <p className="username">Aayush Maharjan</p>
-      <small className="occupation">Full Stack Web developer</small>
+      <small className="occupation">Student</small>
       <small className="website">Maharjanaayush.com</small>
       <div className="email-linkedin-buttons-container">
         <button id="email-btn"><i class="fa-solid fa-envelope"></i> &nbsp;Email</button>
