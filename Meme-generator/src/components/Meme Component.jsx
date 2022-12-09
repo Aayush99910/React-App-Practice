e665426e07da68022987bac6bd3499c4bd0f6245
Aayush99910 <amaharjann01@gmail.com>
2022-12-09 16:00:17 +0545

    adding form and the get new image button is working

--- Meme-generator/src/components/Meme Component.jsx
+++ Meme-generator/src/components/Meme Component.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import memeData from "../data/memeData";
 
 export default function MemeComponent() {
+  const [imageUrl, setImageUrl] = React.useState("https://i.imgflip.com/25w3.jpg");
+
+
+  function getMemeImg() {
+    const memeArray = memeData.data.memes; 
+    let randomNumber = Math.floor(Math.random() * memeArray.length);
+    let {url} = memeArray[randomNumber];
+    setImageUrl(url);
+  }
+
+
   return (
-    <form>
-      <div className="input-container">
-        <input type="text" placeholder="Shut up"></input>
-        <input type="text" placeholder="And take my money"></input>
+    <main>
+      <form>
+        <div className="input-container">
+          <input type="text" placeholder="Top Text"></input>
+          <input type="text" placeholder="Bottom Text"></input>
+        </div>
+        <div className="button-container">
+          <button type="button" onClick={getMemeImg}>Get a new meme image 🖼</button>
+        </div>
+      </form>
+      <div className="meme-image-container">
+        <img src={imageUrl}></img>
       </div>
-      <div className="button-container">
-        <button type="button">Get a new meme image</button>
-      </div>
-    </form>
+    </main>
   )
 }
