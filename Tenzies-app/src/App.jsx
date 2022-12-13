cb124fbb1f06a0049611793a519e08f9dd488cd1
Aayush99910 <amaharjann01@gmail.com>
2022-12-13 14:39:23 +0545

    adding low score and no of rounds function

--- Tenzies-app/src/App.jsx
+++ Tenzies-app/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
+import  Confetti  from "react-confetti";
 
 export default function App() {
   const [tenzies, setTenzies] = useState(false);
   const [dice, setDice] = useState(allNewDice());
+  const [rounds, setRounds] = useState(0);
+  const [lowScore, setLowScore] = useState(JSON.parse(localStorage.getItem("lowScore")) || 0);
 
   function allNewDice() {
     let newArray = [];
@@ -17,11 +20,38 @@ export default function App() {
 
 
   function rollDice() {
-    setDice(oldDice => 
-      oldDice.map(dice => { 
-        return dice.isHeld ? dice : generateNewDice()
-      })
-    )
+    setRounds(prevRoundValue => prevRoundValue + 1)
+    if (tenzies) {
+      setDice(allNewDice());
+      setTenzies(false);
+      setRounds(0);
+      
+      let lowScoreFromLocalStorage = JSON.parse(localStorage.getItem("lowScore"));
+
+      
+      if (typeof lowScoreFromLocalStorage === "number") {
+        if (lowScoreFromLocalStorage === 0) {
+          localStorage.setItem("lowScore", rounds);
+          setLowScore(rounds);
+        }else if (lowScoreFromLocalStorage > rounds){
+          localStorage.setItem("lowScore", rounds);
+          setLowScore(rounds);
+        } else if (lowScoreFromLocalStorage , rounds) {
+          // pass
+        }
+      } else {
+        localStorage.setItem("lowScore", rounds);
+        setLowScore(rounds);
+      } 
+      
+
+    } else {
+      setDice(oldDice => 
+        oldDice.map(dice => { 
+          return dice.isHeld ? dice : generateNewDice()
+        })
+      )
+    }
   }
 
 
@@ -44,21 +74,19 @@ export default function App() {
 
   useEffect(() => {
     function checkWinner() {
+      const allHeld = dice.every(die => die.isHeld);
       let firstNumber = dice[0].value;
-      for (let i = 0; i < dice.length; i++) {
-        const diceValue = dice[i].value;
-        const diceHeld = dice[i].isHeld;
-        if (diceValue !== firstNumber) {
-          return false;
-        } else if (!diceHeld) {
-          return false;
-        }
+      const allSameValue = dice.every(die => die.value === firstNumber)
+      if (allHeld && allSameValue) {
+        return true
       }
-      return true;
+
+      return false
     }
 
-    checkWinner() ? setTenzies(prevValue => !prevValue) : setTenzies(prevValue => prevValue);
+    checkWinner() ? setTenzies(true) : setTenzies(false);
   }, [dice])
+
 
   const diceElements = dice.map((die, index) => {
     return (
@@ -74,17 +102,23 @@ export default function App() {
 
   return (
     <main>
-      <h1 className="title">Tenzies</h1>
+      {tenzies && <Confetti />}
+      <div className="title-container">
+        <h1 className="title">Tenzies</h1>
+        <p className="high-score">Low Score: {lowScore}</p>
+        <p className="rounds">Number of rounds: {rounds}</p>
+      </div>
       <p className="instructions">
         Roll until all dice are the same. 
         Click each die to freeze it at its current value between rolls.
+        To get a low score of all time try to beat the game in less amount of rounds.
       </p>
       <div className="dice-container">
         {diceElements}
       </div>
       <button 
         className="roll-btn" 
-        onClick={rollDice}>{tenzies ? "Reset Game" : "Roll"}</button>
+        onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
 }
